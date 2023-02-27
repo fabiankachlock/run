d36190bd6eda3e003cb5f00125bbe1601ce9d209
fabiankachlock <fabian.kachlock@icloud.com>
2023-02-27 19:04:33 +0100

    fix: silly issue with comp replay word breaks
    This reverts commit 08fd865 (fix: reply?, 2023-02-26).

--- internal/run/completion.go
+++ internal/run/completion.go
@@ -9,7 +9,10 @@ func GenerateCompletionScript() {
 	os.Stdout.Write([]byte(`
 _run_completions()
 {
+  OLD=$COMP_WORDBREAKS
+  COMP_WORDBREAKS=" "
   COMPREPLY=($(compgen -W "$(run --generate-completion-list)" -- "${COMP_WORDS[1]}"))
+  COMP_WORDBREAKS=$OLD
 }
 
 complete -F _run_completions run`))
